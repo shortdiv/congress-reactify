d0a7d31c67012315698679b8e4bd20723d0ab77f
Divya Sasidharan <hello@shortdiv.com>
2016-11-19 14:55:49 -0600

    fix: ajax req works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import config from './config.json'
 
 var CongressMan = React.createClass({
+  getInitialState: function() {
+    return {
+      firstName: '',
+      lastName: ''
+    };
+  },
+  componentDidMount: function() {
+    this.serverRequest = new XMLHttpRequest();
+    this.serverRequest.open('GET', 'https://api.propublica.org/congress/v1/114/senate/members.json', false);
+    this.serverRequest.setRequestHeader("X-API-Key", process.env.congress);
+    this.serverRequest.send()
+    if(this.serverRequest.status === 200) {
+      var data = JSON.parse(this.serverRequest.responseText).results[0]
+        this.data = data;
+      this.setState({
+        firstName: data.chamber,
+        lastName: data.congress
+      })
+    }
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
   render: function() {
     return (
-      <div>{this.props.name}</div>
+      <div>
+      <div>{this.state.firstName}</div>
+      <div>{this.state.lastName}</div>
+      </div>
     );
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CongressMan from './App'
+import CongressMan from './App';
 
 ReactDOM.render(<CongressMan name="Michel" />, document.getElementById('root'));
